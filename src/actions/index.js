21dc380db2ee97a8cda67b10539e041e8d0c234a
Will Malisch <33662180+wmalisch@users.noreply.github.com>
2020-05-07 09:42:07 -0400

    Add files via upload

---
+++ src/actions/index.js
@@ -0,0 +1,23 @@
+import _ from 'lodash'
+import jsonPlaceholder from '../apis/jsonPlaceholder.js';
+
+
+// Can still make normal action creators that reutrn normal actions, in this file
+// But with above action creator, we have to call dispatch manually for the action we are trying to dispatch
+export const fetchPosts = () =>  async dispatch => { 
+    const response = await jsonPlaceholder.get('/posts')
+
+    dispatch({ type: 'FETCH_POSTS', payload: response.data })
+}
+
+export const fetchUser = (id) => dispatch => {
+    _fetchUser(id, dispatch)
+}
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`)
+    dispatch({ type: 'FETCH_USER', payload: response.data})
+})
+
+// if we ever need to access a user moer than once, just remove the memoize func
+
---
+++ src/components/UserHeader.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchUser } from '../actions';
+
+class UserHeader extends React.Component {
+    
+    componentDidMount(){
+        this.props.fetchUser(this.props.userId);
+    }
+
+    render(){
+        const { user } = this.props
+        if(!user){
+            return null
+        }
+        return(
+            <div className='header'>
+                {user.name}
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    // Own props is a reference to the props that are about to be sent to this component
+
+    // Found the individual user in mapStateToProps instead of render, that way the commponent wasnt sent a bulk of data
+    return { user: state.users.find(user => user.id === ownProps.userId) }
+}
+
+
+export default connect(mapStateToProps, { fetchUser })(UserHeader)
